a2acb53c82c5c601909b8b56a4d3331712028146
Sameera S <sameera1595@gmail.com>
2016-03-30 22:44:22 +0530

    fourteen

--- Encryption/src/encryption/Encryption.java
+++ Encryption/src/encryption/Encryption.java
@@ -17,54 +17,58 @@ public class Encryption {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in).useDelimiter("\\n");
-        String ms = in.next();
-        String s = ms.replaceAll(" ", "");
-        double k = Math.sqrt(s.length());
-        int len1 = (int) Math.floor(k);
-        int len2 = (int) Math.ceil(k);
-        int min = 999, r = 0, c = 0;
-        int mk = 0, max;
-        while (len1 * len2 < s.length()) {
-            if (len1 < len2) {
-                len1++;
-            } else if (len2 < k && len2 > len1) {
-                len2++;
+        try {
+            Scanner in = new Scanner(System.in).useDelimiter("\\n");
+            String ms = in.next();
+            String s = ms.replaceAll(" ", "");
+            double k = Math.sqrt(s.length());
+            int len1 = (int) Math.floor(k);
+            int len2 = (int) Math.ceil(k);
+            int min = 999, r = 0, c = 0;
+            int mk = 0, max;
+            while (len1 * len2 < s.length()) {
+                if (len1 < len2) {
+                    len1++;
+                } else if (len2 < k && len2 > len1) {
+                    len2++;
+                }
             }
-        }
-        r = len1;
-        c = len2;
-        int j = 0, i = 0;
-        char mychar[][] = new char[r][c];
-        while (mk < s.length() && i < r) {
-            mychar[i][j++] = s.charAt(mk++);
-            if (j >= c) {
-                j = 0;
-                i++;
+            r = len1;
+            c = len2;
+            int j = 0, i = 0;
+            char mychar[][] = new char[r][c];
+            while (mk < s.length() && i < r) {
+                mychar[i][j++] = s.charAt(mk++);
+                if (j >= c) {
+                    j = 0;
+                    i++;
+                }
             }
-        }
-        String[] column = new String[r];
-        for (i = 0; i < r; i++) {
-            column[i] = String.valueOf(mychar[i]);
-        }
-        for (i = 0; i < c;i++) {
-            for (j = 0; j < r; j++) {
-                if (column[j].indexOf(column[j].charAt(i)) != -1 ) {
-                    System.out.print(column[j].charAt(i));
+            String[] column = new String[r];
+            for (i = 0; i < r; i++) {
+                column[i] = String.valueOf(mychar[i]);
+            }
+
+            for (i = 0; i < c; i++) {
+                for (j = 0; j < r; j++) {
+                    char a = column[j].charAt(i);
+                    int myval = a;
+                    if (a >= 97 && a <= 122) {
+                        System.out.print(column[j].charAt(i));
+                    } else {
+                        break;
+                    }
+                }
+                if (i < c - 1) {
+                    System.out.print(" ");
                 } else {
                     break;
                 }
             }
-            if (i < c - 1) {
-                System.out.print(" ");
-            }
-            else
-            {
-                break;
-            }
-            
+            System.out.println();
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
-        System.out.println();
     }
 
 }
