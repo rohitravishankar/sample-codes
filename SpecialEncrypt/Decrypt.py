b0132dd86d7ee827de4254963a875b0a85faaa80
Sameera S <sameera1595@gmail.com>
2016-05-20 23:15:43 +0530

    twenty three

--- SpecialEncrypt/Decrypt.py
+++ SpecialEncrypt/Decrypt.py
@@ -1,8 +1,9 @@
 from __future__ import division
-s = 930846109532517
+g = float(raw_input())
+numberLetters = int(raw_input())
 myar=[]
 stri = 'acdegilmnoprstuw'
-for k in range(0,9):
+for k in range(0,numberLetters):
 	for i in xrange(len(stri)):
 		pcal = '{0:.2f}'.format(float((g - i)/37))
 		if pcal.split(".")[1]=="00":
