64300e1bba959699b43099475c0eccf016002e30
sameera sy <sameera1595@gmail.com>
2017-06-04 13:46:31 +0530

    twenty seven

--- ShoppingScraper/shop.py
+++ ShoppingScraper/shop.py
@@ -5,6 +5,8 @@ import re
 
 def items_on_page(val):
 	
+		# Counting number of items in each page
+
 		my_elems = val.find('div',{'id' : 'searchResultsContainer'})
 		count=0
 		e = my_elems('div',{'class' : re.compile('.*grid.*')})
@@ -14,18 +16,32 @@ def items_on_page(val):
 
 if len(sys.argv) == 2:
 
-	driver = webdriver.Chrome()
+	# Open the phantomJS browser, driver is the phantomjs browser object
+	driver = webdriver.PhantomJS()
+
+	# With the query given in the command line arguments, open the webpage to find all it's products
 	driver.get("http://shopping.com/products?KW="+sys.argv[1])
+
+	# Get the source code of the page and create a beautifulsoup parser object with it
 	val = BeautifulSoup(driver.page_source,"html.parser")
+
+	# Count the number of objects present in the perticular page
 	count_per_page = items_on_page(val)
+
+	# The pagination concept of moving to next pages, new pages hyperlink given at the end of the page
 	val1 = val.find('div',{'class','paginationNew'})
 	no_of_pages = 1
 	for tag in val1.find_all('a'):
 		if tag.text.replace(" ","").isdigit():
 			no_of_pages = tag.text.replace(" ","")
+
+	# Finally check the number of items in the last page
 	driver.get("http://www.shopping.com/products~PG-"+str(no_of_pages)+"?KW="+sys.argv[1])
 	val = BeautifulSoup(driver.page_source,"html.parser")
 	items_on_last_page = items_on_page(val)
+
+	# Until the last page, you are sure of the number of elements occuring in each page. So multiply (count of items * No. of pages) + (count of items in last page)
+
 	print "The total items ",(int((int(no_of_pages) - 1))*int(count_per_page)) + int(items_on_last_page)
 	driver.quit()
 
